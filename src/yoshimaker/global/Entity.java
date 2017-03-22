d2e0323307b202e49fee0e321cf39e6372cc0b8b
bloudman <adrien@luneau.me>
2017-03-22 15:46:28 +0100

    entity

---
+++ src/yoshimaker/global/Entity.java
@@ -0,0 +1,18 @@
+package yoshimaker.global;
+
+import org.newdawn.slick.Image;
+
+/**
+ * Class entity.
+ * Contain X, Y and an image
+ * Created by adrien on 22/03/2017.
+ */
+public class Entity {
+    private int x;
+    private int y;
+    private Image image;
+
+    public Entity(Image image, int x, int y){
+        
+    }
+}
