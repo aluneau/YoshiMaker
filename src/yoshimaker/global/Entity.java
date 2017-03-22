22e4e4d8d44c76205ec6e41d48cc560221069bee
bloudman <adrien@luneau.me>
2017-03-22 15:57:12 +0100

    modify gitignore

--- src/yoshimaker/global/Entity.java
+++ src/yoshimaker/global/Entity.java
@@ -1,5 +1,6 @@
 package yoshimaker.global;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 
 /**
@@ -10,9 +11,39 @@ import org.newdawn.slick.Image;
 public class Entity {
     private int x;
     private int y;
-    private Image image;
+    private Animation sprite;
 
-    public Entity(Image image, int x, int y){
-        
+    public Entity(Animation sprite, int x, int y){
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Animation getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Animation sprite) {
+        this.sprite = sprite;
+    }
+
+    public void draw(int x, int y){
+        sprite.draw(x, y);
     }
 }
