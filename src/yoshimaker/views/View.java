34ccf351b8a20207083909a7041cecfb5cc6d573
Lowlighter <simon.lecoq@live.fr>
2017-03-22 15:38:43 +0100

    Updated abstract view class signature

--- src/yoshimaker/views/View.java
+++ src/yoshimaker/views/View.java
@@ -5,6 +5,8 @@
  */
 package yoshimaker.views;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 /**
@@ -15,17 +17,22 @@ public abstract class View {
 
     /**
      * Crée les resources nécessaires à l'affichage de la vue
+     * @param container
+     * @throws org.newdawn.slick.SlickException
      */
-    public abstract void init() throws SlickException;
+    public abstract void init(GameContainer container) throws SlickException;
 
     /**
      * Redessine la vue
+     * @param container
+     * @param g
      */
-    public abstract void render();
+    public abstract void render(GameContainer container, Graphics g);
 
     /**
      * Met à jour le contenu de la vue
+     * @param container
      * @param delta
      */
-    public abstract void update(int delta);
+    public abstract void update(GameContainer container, int delta);
 }
