8e1ff8c03e3042b7a994d273311034bbee1d073e
Lowlighter <simon.lecoq@live.fr>
2017-03-22 01:35:03 +0100

    Added new super titlescreen deluxe 2 premium GOTY reloaded

--- src/yoshimaker/WindowGame.java
+++ src/yoshimaker/WindowGame.java
@@ -4,7 +4,8 @@ import org.newdawn.slick.*;
 
 public class WindowGame extends BasicGame {
     private GameContainer container;
-
+    private TitleScreen view;
+    
     public WindowGame() {
         super("Lesson 1 :: WindowGame");
     }
@@ -12,14 +13,18 @@ public class WindowGame extends BasicGame {
     @Override
     public void init(GameContainer container) throws SlickException {
         this.container = container;
+        view = new TitleScreen();
+        view.init();
     }
 
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
+        view.render();
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
+        view.update(delta);
     }
 
     @Override
