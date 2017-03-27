e4ab3e2de1b0db4917e831d4b97e95c6e2ada150
Adrien <adrien@MBP-de-Adrien.lan>
2017-03-27 22:09:02 +0200

    update gitignore

--- src/yoshimaker/global/Physics.java
+++ src/yoshimaker/global/Physics.java
@@ -5,14 +5,11 @@
  */
 package yoshimaker.global;
 
-import java.awt.Rectangle;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyDef;
-import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.FixtureDef;
-import org.jbox2d.dynamics.World;
+import org.jbox2d.dynamics.*;
+
+import java.awt.*;
 
 /**
  *
